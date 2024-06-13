87c1ab513af85c22890c7a4d8587d52f48c102d1
Chamaali <chamaalidilka@gmail.com>
2024-06-13 09:16:05 +0530

    Navigation Bar was Styled.

--- app/components/home/HomeColumnOne.tsx
+++ app/components/home/HomeColumnOne.tsx
@@ -13,21 +13,23 @@ const HomeColumnOne = () => {
             "url('https://res.cloudinary.com/dmwk6ybet/image/upload/v1714157388/Lakshmi%20Ayurweda/Background%20Images/bg-1_qlqfua.jpg')",
         }}
       >
-        <div className="container ">
+        <div className="container">
           <div className="flex flex-row py-4">
-            <div className="flex-column bg-white  rounded-xl  w-48 h-48 mx-3 p-3 content-center hover:scale-105 hover:bg-gray-100 transition duration-300 ease-in-out">
-              <div className="grid justify-items-center gap-4 ">
-                <Image
-                  className=""
-                  alt=""
-                  src={require("@/public/icons/Book Icon.png")}
-                  width={50}
-                />
-                <p className="text-green-700 font-serif text-lg text-center">
-                  Book an Appointment
-                </p>
+            <a href=""  className="no-underline ">
+              <div className="flex-column bg-white  rounded-xl  w-48 h-48 mx-3 p-3 content-center hover:scale-105 hover:bg-gray-100 transition duration-300 ease-in-out">
+                <div className="grid justify-items-center gap-4 ">
+                  <Image
+                    className=""
+                    alt=""
+                    src={require("@/public/icons/Book Icon.png")}
+                    width={50}
+                  />
+                  <p className="text-green-700 font-serif text-lg text-center">
+                    Book an Appointment
+                  </p>
+                </div>
               </div>
-            </div>
+            </a>
             <a href="../../pages/locateClinic" className="no-underline ">
               <div className="flex-column bg-white  rounded-xl  w-48 h-48 mx-3 p-3 content-center hover:scale-105 hover:bg-gray-100 transition duration-300 ease-in-out">
                 <div className="grid justify-items-center gap-4 ">
@@ -45,20 +47,22 @@ const HomeColumnOne = () => {
             </a>
           </div>
           <div className="flex flex-row py-4">
-            <div className="flex-column bg-white  rounded-xl  w-48 h-48 mx-3 p-3 content-center hover:scale-105 hover:bg-gray-100 transition duration-300 ease-in-out">
-              <div className="grid justify-items-center gap-4 ">
-                <Image
-                  className=""
-                  alt=""
-                  src={require("../../../public/icons/Wellness Icon.png")}
-                  width={60}
-                />
-                <p className="text-green-700 font-serif  text-lg text-center">
-                  Panchakarma Therapy
-                </p>
+            <a href=""  className="no-underline ">
+              <div className="flex-column bg-white  rounded-xl  w-48 h-48 mx-3 p-3 content-center hover:scale-105 hover:bg-gray-100 transition duration-300 ease-in-out">
+                <div className="grid justify-items-center gap-4 ">
+                  <Image
+                    className=""
+                    alt=""
+                    src={require("../../../public/icons/Wellness Icon.png")}
+                    width={60}
+                  />
+                  <p className="text-green-700 font-serif  text-lg text-center">
+                    Panchakarma Therapy
+                  </p>
+                </div>
               </div>
-            </div>
-            <a href="./../services/buyOnlineProducts">
+            </a>
+            <a href="./../services/buyOnlineProducts" className="no-underline ">
               <div className="flex-column bg-white  rounded-xl  w-48 h-48 mx-3 p-3 content-center hover:scale-105 hover:bg-gray-100 transition duration-300 ease-in-out">
                 <div className="grid justify-items-center gap-4 ">
                   <Image
@@ -67,7 +71,7 @@ const HomeColumnOne = () => {
                     src={require("../../../public/icons/Buy Icon.png")}
                     width={70}
                   />
-                  <p className="text-green-700 font-serif  text-lg text-center">
+                  <p className="text-green-700 font-serif  text-lg text-center ">
                     Buy Products
                   </p>
                 </div>
--- app/components/navigationBar/page.tsx
+++ app/components/navigationBar/page.tsx
@@ -2,61 +2,56 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Offcanvas from "react-bootstrap/Offcanvas";
 import { FaUser } from "react-icons/fa";
 import SignInForm from "../SignInForm";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const diseases = [
   {
     id: 1,
     name: "Back Pain",
-    href: "./../diseases/BackPain",
+    href: "/diseases/BackPain",
   },
   {
     id: 2,
     name: "Bulging of Rectum",
-    href: "./../diseases/BulgingOfRectum",
+    href: "/diseases/BulgingOfRectum",
   },
   {
     id: 3,
     name: "Endocrine",
-    href: "./endrocrine",
+    href: "/diseases/endocrine",
   },
   {
     id: 4,
     name: "Central nerves system",
-    href: "./centralNervesSystem",
+    href: "/diseases/centralNervesSystem",
   },
   {
     id: 5,
     name: "Blood circulatory system",
-    href: "./bloodCirculatorySystem",
+    href: "/diseases/bloodCirculatorySystem",
   },
   {
     id: 6,
     name: "Urinary system",
-    href: "./urinarySystem",
+    href: "/diseases/urinarySystem",
   },
   {
     id: 7,
     name: "Hair & skin",
-    href: "./hairSkin",
+    href: "/diseases/hairSkin",
   },
   {
     id: 8,
     name: "Reproductive system",
-    href: "./reproductiveSystem",
+    href: "/diseases/reproductiveSystem",
   },
   {
     id: 9,
     name: "Liver disease",
-    href: "./liverDisease",
+    href: "/diseases/liverDisease",
   },
 ];
 
@@ -64,160 +59,184 @@ const services = [
   {
     id: 1,
     name: "Phone Consultation",
-    href: "./phoneConsultation",
+    href: "/services/phoneConsultation",
   },
   {
     id: 2,
     name: "Video Consultation",
-    href: "./videoConsultation",
+    href: "/services/videoConsultation",
   },
   {
     id: 3,
     name: "Clinic Consultation",
-    href: "./clinicConsultation",
+    href: "/services/clinicConsultation",
   },
   {
     id: 4,
     name: "Buy Online Products",
-    href: "./buyOnlineProducts",
+    href: "/services/buyOnlineProducts",
   },
   {
     id: 5,
     name: "Ayurweda for Wellness",
-    href: "./ayurwedaForWellness",
+    href: "/services/ayurwedaForWellness",
   },
   {
     id: 6,
-    name: "Find prakurthi and Body Constitution",
-    href: "./findPrakurthiAndBodyConsultation", //
+    name: "Find Prakurthi and Body Constitution",
+    href: "/services/findPrakurthiAndBodyConsultation",
   },
   {
     id: 7,
     name: "Yoga Meditation",
-    href: "./yogaMeditation",
+    href: "/services/yogaMeditation",
   },
   {
     id: 8,
     name: "Ayurweda Beauticulture",
-    href: "./ayurwedaBeauticulture",
+    href: "/services/ayurwedaBeauticulture",
   },
   {
     id: 9,
-    name: "Acupuncture and Cupping",
-    href: "./acupunctureAndCupping",
+    name: "Acupuncture and Cupping",
+    href: "/services/acupunctureAndCupping",
   },
 ];
 
 export default function Page() {
+
+    const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const togglePopup = () => {
     setIsOpen(!isOpen);
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  
+
+ 
   return (
-    <div className="text-white !important">
-      {["md"].map((expand) => (
-        <Navbar
-          key={expand}
-          expand={expand}
-          className=" text-white !important bg-green-900  px-2 "
-        >
-          <Container fluid className="text-white !important">
-            <Navbar.Brand href="./" className="pl-3 text-white !important">
-              <Image
-                src={require("@/public/images/LogoForNavigationBar.png")}
-                alt=""
-                width={100}
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle
-              aria-controls={`offcanvasNavbar-expand-${expand}`}
-              className="text-white !important"
+    <div className="text-white sticky top-0 right-0 left-0 bg-green-900 py-2 px-5 z-10">
+      <nav className="flex items-center justify-between py-4 ">
+        <div className="pl-3">
+          <a href="/">
+            <Image
+              src={require("@/public/images/LogoForNavigationBar.png")}
+              alt="Logo"
+              width={90}
             />
-            <Navbar.Offcanvas
-              className="text-white !important"
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
+          </a>
+        </div>
+        <div className="hidden md:flex items-center space-x-4">
+        <div className="relative group">
+  <button className="font-semibold font-sans bold text-base px-4 py-2">Diseases</button>
+  <ul className="absolute hidden group-hover:block p-3 bg-white bg-opacity-30 text-green-700 rounded shadow-lg w-60">
+    {diseases.map((diseases) => (
+      <li key={diseases.id}>
+        <a className="text-green-700 no-underline" href={diseases.href}>
+          {diseases.name}
+        </a>
+        <hr />
+      </li>
+    ))}
+  </ul>
+</div>
+          <Link legacyBehavior href="/whyLakshmiAyurweda">
+            <a className={`${pathname === '/whyLakshmiAyurweda' ? "text-slate-300" : "text-white"} font-sans  font-semibold no-underline`}>
+              Why Lakshmi Ayurweda
+            </a>
+          </Link>
+          <div className="relative group">
+  <button className="font-semibold font-sans text-base px-4 py-2">Our Services</button>
+  <ul className="absolute hidden group-hover:block p-3 bg-white bg-opacity-30 text-green-700 rounded shadow-lg w-60">
+    {services.map((service) => (
+      <li key={service.id}>
+        <a className="text-green-700 no-underline" href={service.href}>
+          {service.name}
+        </a>
+        <hr />
+      </li>
+    ))}
+  </ul>
+</div>
+          <Link legacyBehavior href="/contactUs">
+            <a className={`${pathname === '/contactUs' ? "text-slate-300" : "text-white"} font-sans font-semibold no-underline`}>
+              Contact Us
+            </a>
+          </Link>
+        </div>
+        <div className="flex items-center  gap-x-9">
+        <Link href="/bookAppointment" className="py-2 px-3  bg-slate-100 text-green-700 font-bold rounded shadow hover:bg-slate-200">
+        <button>
+          Book Appointment
+        </button>
+     
+    </Link>
+          <button type="button" onClick={togglePopup}>
+            <FaUser size={23} />
+          </button>
+          {isOpen && <SignInForm />}
+          <button className="md:hidden text-white" onClick={toggleMenu}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body className="text-white !important">
-                <Nav className="justify-content-end flex-grow-1 pe-3 text-white !important">
-                  <div className="text-white !important">
-                    <NavDropdown
-                      title="Diseases"
-                      id={`offcanvasNavbarDropdown-expand-${expand}`}
-                    >
-                      {diseases.map((disease) => (
-                        <NavDropdown.Item
-                          key={disease.id}
-                          href={disease.href}
-                          className="no-underline hover:underline "
-                        >
-                          {disease.name}
-                        </NavDropdown.Item>
-                      ))}
-                    </NavDropdown>
-                  </div>
-
-                  <Nav.Link
-                    href="./whyLakshmiAyurweda"
-                    className="text-white  text-lg font-bold !important"
-                  >
-                    Why Lakshmi Ayurweda
-                  </Nav.Link>
-
-                  <NavDropdown
-                    title="Our Services"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                    className="text-white !important"
-                  >
-                    {services.map((service) => (
-                      <NavDropdown.Item
-                        key={service.id}
-                        href={service.href}
-                        className="no-underline hover:underline text-green-700 !important"
-                      >
-                        {service.name}
-                      </NavDropdown.Item>
-                    ))}
-                  </NavDropdown>
-
-                  <Nav.Link href="./contactUs" className="text-white  ">
-                    Contact Us
-                  </Nav.Link>
-                </Nav>
-                <Form className="flex flex-nowrap gap-4">
-                  <div>
-                    <Button variant="outline-success" href="./bookAppointment">
-                      Book Appointment
-                    </Button>
-                  </div>
-                  <div>
-                    <button type="button" onClick={togglePopup}>
-                      <FaUser className=" self-end" size={23} />
-                    </button>
-                    {isOpen && <SignInForm />}
-                  </div>
-
-                  {/* <div>
-                    <Button variant="outline-success" href="./admin">
-                      Admin
-                    </Button>
-                  </div> */}
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </nav>
+      {menuOpen && (
+        <div className="md:hidden bg-green-900 text-white space-y-4 py-4">
+          <div className="relative group">
+            <button className="block w-full text-left px-4 py-2">
+              Diseases
+            </button>
+            <div className="absolute bg-white text-green-700 rounded shadow-lg">
+              {diseases.map((disease) => (
+                <Link legacyBehavior key={disease.id} href={disease.href}>
+                  <a className={`${pathname === '{disease.href}' ? "text-blue-500" : "text-black"} font-bold no-underline`}>
+                    {disease.name}
+                  </a>
+                </Link>
+              ))}
+            </div>
+          </div>
+          <Link legacyBehavior href="/whyLakshmiAyurweda">
+            <a className={`${pathname === '/whyLakshmiAyurweda' ? "text-blue-500" : "text-black"} font-bold no-underline`}>Why Lakshmi Ayurweda</a>
+          </Link>
+          <div className="relative group">
+            <button className="block w-full text-left px-4 py-2">
+              Our Services
+            </button>
+            <div className="absolute bg-white text-green-700 rounded shadow-lg">
+              {services.map((service) => (
+                <Link legacyBehavior key={service.id} href={service.href}>
+                  <a className={`${pathname === '{service.href}' ? "text-blue-500" : "text-black"} font-bold no-underline`}>
+                    {service.name}
+                  </a>
+                </Link>
+              ))}
+            </div>
+          </div>
+          <Link legacyBehavior href="/contactUs">
+            <a className={`${pathname === '/contactUs' ? "text-blue-500" : "text-black"} font-bold no-underline`}>Contact Us</a>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
-
-//change color of the nav items
