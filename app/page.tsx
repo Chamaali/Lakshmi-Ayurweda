fda532317dbadd4b5f473549d683c1020b627609
Chamaali <chamaalidilka@gmail.com>
2024-06-17 19:02:11 +0530

    Updated npm packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
 import HomeColumnTwo from './components/home/HomeColumnTwo'
 import HomeColumnFour from './components/home/HomeColumnFour'
 import HomeColumnThree from './components/home/HomeColumnThree'
-// import HomeColumnFive from './components/home/HomeColumnFive'
-// import HomeColumnSix from './components/home/HomeColumnSix'
 import HomeColumnSeven from './components/home/HomeColumnSeven'
 import HomeColumnOne from './components/home/HomeColumnOne'
 import { HomeColumnFive } from './components/home/HomeColumnFive'
-// import HomeColumnFive from './components/home/HomeColumnFive'
-// import { GetAllProducts } from './serverComponents/products/GetAllProducts'
-// import { GetAllDoctors } from './serverComponents/products/GetAllDoctors'
-// import HomeColumnFive from './components/home/HomeColumnFive'
-// import HomeColumnFive from './serverComponents/products/GetAllProducts'
-// import HomeColumnFive from './components/home/HomeColumnFive'
-// import { HomeColumnFive } from './components/home/App'
-// import HomeColumnFive from './components/home/HomeColumnFive'
-
 
 
 
@@ -26,9 +15,6 @@ const HomePage= () => {
         <HomeColumnThree />
         <HomeColumnFour />
         <HomeColumnFive />
-        {/* <HomeColumnFive /> */}
-        {/* <GetAllProducts/> */}
-        {/* <GetAllDoctors /> */}
        
         <HomeColumnSeven />    
     </div>
