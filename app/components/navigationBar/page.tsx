d9e11f395b742324ec1cb4026b7b772958cf3698
DineshPriyantha <priyanthadinesh2002@gmail.com>
2024-09-06 18:33:42 +0530

    Up:palpitation frontend with model

--- app/components/navigationBar/page.tsx
+++ app/components/navigationBar/page.tsx
@@ -323,7 +323,7 @@ export default function Page() {
                               Hypertension
                             </a>
                             <a
-                              href="#"
+                              href="./../../diseases/Palpitation"
                               className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100"
                               role="menuitem"
                               id="submenu-item-5"
