5cc752fea0f26df0d3b9044e6e5efba87bf7a994
JIshanRandika <jishanrandika@gmail.com>
2024-11-22 17:06:36 +0530

    Update model

--- models/appointment.js
+++ models/appointment.js
@@ -3,13 +3,13 @@ import mongoose, {Schema} from "mongoose";
 const appointmentSchema = new Schema(
     {
         name: String,
-        age:   String,
+        age: Number,
         Gender: String,
         phone: String,
         email: String,
         typeOfConsultation: String,
         typeofPackage: Boolean,
-        totalAmount:   String,
+        totalAmount: String,
         url: String,
         checked: { type: Boolean, default: false },
     },
